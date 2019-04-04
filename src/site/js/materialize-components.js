0bd22dad3d2b9b571ab0877d9ac5bdd0020b41e0
Meto Trajkovski <meto.trajkovski@3shape.com>
2019-04-04 20:49:22 +0200

    Copy from SlediPratki app

---
+++ src/site/js/materialize-components.js
@@ -0,0 +1,29 @@
+(function (global) {
+    global.MaterializeComponents = {
+        tabsInstance: undefined,
+        activeInstance: undefined,
+        archiveInstance: undefined,
+        floatingButtonInstance: undefined,
+        refreshIntervalInstance: undefined,
+        addModal: {
+            addModalInstance: undefined,
+            trackingNumberInput: undefined,
+            trackingNumberLabel: undefined,
+            packageDescriptionLabel: undefined,
+            addSpinner: undefined
+        },
+        refreshModal: {
+            refreshModalInstance: undefined,
+            refreshSpinner: undefined
+        },
+        editModal: {
+            editModalInstance: undefined,
+            packageDescriptionInput: undefined,
+            packageDescriptionLabel: undefined
+        },
+        actionModalInstance: undefined,
+        leftTooltipInstances: undefined,
+        scrollableContent: undefined,
+        mainSpinner: undefined
+    };
+}(this));

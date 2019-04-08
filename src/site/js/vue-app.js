068cd5f90a036e5e263d0b9da57bf743b4c094fd
Meto Trajkovski <meto.trajkovski@3shape.com>
2019-04-08 20:30:10 +0200

    Completed poll info section

--- src/site/js/vue-app.js
+++ src/site/js/vue-app.js
@@ -16,12 +16,22 @@ new Vue({
             var scrollspy = document.querySelectorAll('.scrollspy');
             M.ScrollSpy.init(scrollspy);
 
-            // remove the spinner after all pictures are loaded
-            var spinner = document.querySelector('#spinner');
-            spinner.style.display = "none";
+            // remove the main spinner after all of the content is loaded
+            var mainSpinner = document.querySelector('#main-spinner');
+            mainSpinner.style.display = "none";
             
             var body = document.querySelector('body');
             body.classList.remove("spinner-loading");
+
+            var timeElems = document.querySelectorAll('.timepicker');
+            var timeInstances = M.Timepicker.init(timeElems, {
+                'twelveHour': false
+            });
+
+            var dateElems = document.querySelectorAll('.datepicker');
+            var dateInstances = M.Datepicker.init(dateElems, {
+                'format': 'dddd (dd.mm.yyyy)'
+            });
         })
     },
     watch: {
